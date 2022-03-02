948c2e0b19ad6b964ffe72c8cc00ae10e1daa2a0
Patel-Preet <pp225622@gmail.com>
2022-03-02 16:32:51 +0800

    Done

--- Section 7/app/src/main/java/com/example/activitylifecycle/MainActivity.kt
+++ Section 7/app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.activitylifecycle
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -21,6 +23,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         Log.d(TAG, "onCreate() called")
 
